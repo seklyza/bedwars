5e100e655c903d2c441002f872f98df92aea8b07
seklyza <shachar.benda@gmail.com>
2020-04-13 16:14:28 +0300

    Preventing bed interaction and breaking empty teams' beds

--- src/main/java/com/seklyza/bedwars/events/PlayerInteract.kt
+++ src/main/java/com/seklyza/bedwars/events/PlayerInteract.kt
@@ -1,7 +1,9 @@
 package com.seklyza.bedwars.events
 
 import com.seklyza.bedwars.game.PlayerState
+import org.bukkit.block.data.type.Bed
 import org.bukkit.event.EventHandler
+import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 
 class PlayerInteract : Event() {
@@ -11,5 +13,6 @@ class PlayerInteract : Event() {
         val gp = game.players[e.player]
 
         if (gp?.state != PlayerState.PLAYER) e.isCancelled = true
+        if(e.clickedBlock?.blockData is Bed && e.action == Action.RIGHT_CLICK_BLOCK) e.isCancelled = true
     }
 }
